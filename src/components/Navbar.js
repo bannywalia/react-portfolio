0066332b9357f7ba514c7818b3dce42b6703101f
Bannywalia <Banny.walia07@gmail.com>
2023-01-19 12:57:46 -0500

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import "./Navbar.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/"> Home</Link>
+        </li>
+        <li>
+          <Link to="/project"> Project</Link>
+        </li>
+        <li>
+          <Link to="/about"> About</Link>
+        </li>
+        <li>
+          <Link to="/contact"> Contact</Link>
+        </li>
+      </ul>
+      <div className="dropdown" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
