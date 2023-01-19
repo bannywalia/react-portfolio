075ab361c0e8e4e5d5bef5cf061da914a7baa992
Bannywalia <Banny.walia07@gmail.com>
2023-01-19 15:25:41 -0500

    adding work cards

---
+++ src/components/workcard.jsx
@@ -0,0 +1,14 @@
+import "./workcard.css";
+
+export const workcard = () => {
+  return (
+    <div className="work-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card"></div>
+      </div>
+    </div>
+  );
+};
+
+export default workcard;
